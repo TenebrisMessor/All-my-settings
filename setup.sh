f629c6022a56c470c9714160b3f7264f431264d4
TenebrisMessor <arielhryo@hotmail.com>
2025-06-23 03:01:03 +0000

    adding new structre

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "🧠 Detectando sistema operativo..."
+OS="$(uname)"
+
+case "$OS" in
+  "Linux")
+    echo "🐧 Linux detectado"
+    bash linux/setup-linux.sh
+    ;;
+
+  "Darwin")
+    echo "🍎 MacOS detectado"
+    bash macos/setup-macos.sh
+    ;;
+
+  MINGW* | MSYS* | CYGWIN*)
+    echo "🪟 Windows detectado"
+    powershell.exe -File windows/setup-windows.ps1
+    ;;
+
+  *)
+    echo "❌ SO no reconocido"
+    ;;
+  esac
